731f37ac511fcf54e8a8c0f60c26e501aa862dc0
Denis Strizhkin <strdenis02@gmail.com>
2023-04-01 19:56:54 +0300

    finished run script

--- new_run/test_run.sh
+++ new_run/test_run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-lmp -in in.fall \
+mpirun -np 10 lmp \
+  -in in.fall \
+  -suffix gpu \
+  -package gpu 0 \
+  \
   -var input_file '../input_files/fall0.input.data' \
   -var mol_file '../input_files/mol.C60' \
+  -var elstop_table '../input_files/elstop-table.txt ' \
   \
   -var lattice 5.43 \
   \
@@ -11,3 +16,8 @@ lmp -in in.fall \
   -var C60_x 0 \
   -var C60_y 0 \
   -var C60_z_offset 30 \
+  \
+  -var step 0.001 \
+  -var temperature 0 \
+  -var fall_steps 5000 \
+  -var energy 2 \
