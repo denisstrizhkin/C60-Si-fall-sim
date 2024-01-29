209991c36fa469e95ef59bada30f8f95cdc20938
Denis Strizhkin <strdenis02@gmail.com>
2024-01-29 03:25:24 +0300

    rerun example

--- new_run/rerun_example.sh
+++ new_run/rerun_example.sh
@@ -2,21 +2,19 @@
 
 export LAMMPS_POTENTIALS=/usr/share/lammps/potentials
 
-input_file="../input_files/fall_700K_40_40_31.input.data"
+input_dir="../results/multifall_width_40_700K_40_40_8keV_300/run_300/"
+input_file="${input_dir}/input.data"
 
 ./run.py \
   --omp-threads 0 \
   --mpi-cores 8 \
   \
-  --temperature "700" \
-  --energy 8 \
   --runs 600 \
-  --run-time 5000 \
-  \
   --results-dir '../results/multifall_width_40_700K_40_40_8keV_300' \
   \
   --input-file "${input_file}" \
   --mol-file '../input_files/mol.C60' \
   --estop-table '../input_files/elstop-table.txt' \
   \
-  --script-dir './'
+  --script-dir './' \
+  --input-vars "${input_dir}/vars.json"
