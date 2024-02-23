b8305f37ff117ebc7b9de8d4f8f1ad9a561e9aea
Denis Strizhkin <strdenis02@gmail.com>
2024-02-23 22:24:48 +0300

    get rid of hardcode in vel.py

--- new_run/vel.py
+++ new_run/vel.py
@@ -3,17 +3,30 @@ import math
 energy = 8
 vel = math.sqrt(energy * 1000) * 5.174
 
-cos_8 = math.cos(math.radians(8))
-cos_82 = math.cos(math.radians(82))
+angle_1 = 16
+angle_2 = 90 - angle_1
 
-vel_z = vel * cos_8
-vel_x = vel * cos_82
+cos_1 = math.cos(math.radians(angle_1))
+cos_2 = math.cos(math.radians(angle_2))
 
-z = 100
-x = z * (vel_x / vel_z)
+vel_z = vel * cos_1
+vel_x = vel * cos_2
 
-print(f"cos 8: {cos_8}")
-print(f"cos 82: {cos_82}")
+z = 150
+lattice = 5.43
+
+x0 = z * (vel_x / vel_z)
+
+zlo = lattice * 6.5
+zhi = zlo + lattice * 14
+
+x_offset = lattice * 12
+
+xlo = x_offset - x0 * (z - zlo) / z
+xhi = x_offset - x0 * (z - zhi) / z
+
+print(f"cos {angle_1}: {cos_1}")
+print(f"cos {angle_2}: {cos_2}")
 print("---")
 print(f"vel: {vel}")
 print("---")
@@ -21,4 +34,5 @@ print(f"vel z: {vel_z}")
 print(f"vel x: {vel_x}")
 print("---")
 print(f"z: {z}")
-print(f"x: {x}")
+print(f"xlo: {xlo}")
+print(f"xhi: {xhi}")
