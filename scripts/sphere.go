0044ec923fc173233509ad9519b50ef1eba18066
Denis Strizhkin <strdenis02@gmail.com>
2024-07-18 01:50:34 +0300

    ready any dump keys

--- scripts/sphere.go
+++ scripts/sphere.go
@@ -2,22 +2,102 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"math"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
 )
 
-type Atom struct {
-	type_       int
-	x, y, z, ek float32
+type Dump struct {
+	data      map[string]map[int][]float32
+	timesteps []int
 }
 
-type Dump struct {
-	timesteps map[int][]Atom
+func NewDump(path string) Dump {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalln("reading dump file:", path, err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	dump := Dump{
+		data:      make(map[string]map[int][]float32),
+		timesteps: make([]int, 0),
+	}
+	keys := make([]string, 0)
+
+	is_read_atom_count := false
+	is_read_atoms := false
+	is_read_timestep := false
+
+	atom_count := -1
+	current_atom := -1
+	current_timestep := -1
+	for scanner.Scan() {
+		if scanner.Text() == "ITEM: TIMESTEP" {
+			is_read_atoms = false
+			current_atom = -1
+			is_read_timestep = true
+			continue
+		}
+
+		if is_read_timestep {
+			is_read_timestep = false
+			current_timestep = parse_int(scanner.Text())
+			dump.timesteps = append(dump.timesteps, current_timestep)
+			// if current_timestep == 500 {
+			// 	return dump
+			// }
+			log.Println("reading timestep:", current_timestep)
+			continue
+		}
+
+		if scanner.Text() == "ITEM: NUMBER OF ATOMS" {
+			is_read_atom_count = true
+			continue
+		}
+
+		if is_read_atom_count {
+			atom_count = parse_int(scanner.Text())
+			is_read_atom_count = false
+			continue
+		}
+
+		if strings.Contains(scanner.Text(), "ITEM: ATOMS") {
+			is_read_atoms = true
+			if len(keys) == 0 {
+				for _, key := range strings.Split(scanner.Text(), " ")[2:] {
+					keys = append(keys, key)
+					dump.data[key] = make(map[int][]float32)
+				}
+			}
+
+			for _, val := range dump.data {
+				val[current_timestep] = make([]float32, atom_count)
+			}
+			continue
+		}
+
+		if is_read_atoms {
+			tokens := strings.Split(scanner.Text(), " ")
+			current_atom++
+			for i, key := range keys {
+				dump.data[key][current_timestep][current_atom] = parse_float(tokens[i])
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalln("reading dump file:", err)
+	}
+
+	return dump
+}
+
+func (d Dump) extract(field string, timestep int) []float32 {
+	return d.data[field][timestep]
 }
 
 func parse_float(s string) float32 {
@@ -36,102 +116,38 @@ func parse_int(s string) int {
 	return i
 }
 
-func read_dump(dump_path string) Dump {
-	file, err := os.Open(dump_path)
-	if err != nil {
-		log.Fatalln("reading dump file: ", dump_path, err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	dump := Dump{
-		timesteps: make(map[int][]Atom),
-	}
-
-	is_read_atom_count := false
-	is_read_atoms := false
-	is_read_timestep := false
-
-	current_atom := -1
-	current_timestep := -1
-	for scanner.Scan() {
-		if scanner.Text() == "ITEM: TIMESTEP" {
-			is_read_atoms = false
-			current_atom = -1
-			is_read_timestep = true
-			continue
-		}
-
-		if is_read_timestep {
-			is_read_timestep = false
-			current_timestep = parse_int(scanner.Text())
-			// if current_timestep == 500 {
-			// 	return dump
-			// }
-			log.Println("reading timestep:", current_timestep)
-			continue
-		}
-
-		if scanner.Text() == "ITEM: NUMBER OF ATOMS" {
-			is_read_atom_count = true
-			continue
-		}
-
-		if is_read_atom_count {
-			atom_count := parse_int(scanner.Text())
-			dump.timesteps[current_timestep] = make([]Atom, atom_count)
-			is_read_atom_count = false
-			continue
-		}
-
-		if strings.Contains(scanner.Text(), "ITEM: ATOMS") {
-			is_read_atoms = true
-			continue
-		}
-
-		if is_read_atoms {
-			// id type x y z vx vy vz c_atom_ke
-			tokens := strings.Split(scanner.Text(), " ")
-			current_atom++
-			dump.timesteps[current_timestep][current_atom].type_ = parse_int(tokens[1])
-			dump.timesteps[current_timestep][current_atom].x = parse_float(tokens[2])
-			dump.timesteps[current_timestep][current_atom].y = parse_float(tokens[3])
-			dump.timesteps[current_timestep][current_atom].z = parse_float(tokens[4])
-			dump.timesteps[current_timestep][current_atom].ek = parse_float(tokens[8])
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
-	}
-
-	return dump
-}
-
 func calc_zero_lvl(dump Dump, timestep int) float32 {
 	var zero_lvl float32 = math.SmallestNonzeroFloat32
-	for _, atom := range dump.timesteps[timestep] {
-		if atom.z > zero_lvl && atom.type_ == 1 {
-			zero_lvl = atom.z
+	z := dump.extract("z", timestep)
+	type_ := dump.extract("type", timestep)
+
+	for i := range z {
+		if z[i] > zero_lvl && type_[i] == 1 {
+			zero_lvl = z[i]
 		}
 	}
+
 	return zero_lvl
 }
 
 func calc_cluster_center(dump Dump, timestep int) (float32, float32) {
-	var x, y float32 = 0, 0
-	count := 0
-	for _, atom := range dump.timesteps[timestep] {
-		if atom.type_ == 2 {
-			x += atom.x
-			y += atom.y
+	var c_x, c_y, count float32
+	x := dump.extract("x", timestep)
+	y := dump.extract("y", timestep)
+	type_ := dump.extract("type", timestep)
+
+	for i := range x {
+		if type_[i] == 2 {
+			c_x += x[i]
+			c_y += y[i]
 			count++
 		}
 	}
-	return x / float32(count), y / float32(count)
+
+	return c_x / count, c_y / count
 }
 
-func sphere_tables(dump Dump, run_dir string, zero_lvl float32, x float32, y float32) {
+func sphere_tables(dump Dump, run_dir string, zero_lvl, c_x, c_y float32) {
 	table_path := run_dir + "/sphere_ek.txt"
 	table, err := os.Create(table_path)
 	if err != nil {
@@ -148,28 +164,26 @@ func sphere_tables(dump Dump, run_dir string, zero_lvl float32, x float32, y flo
 	}
 	table_str[0][0] = "timestep\\R"
 
-	timesteps := make([]int, 0)
-	for timestep := range dump.timesteps {
-		timesteps = append(timesteps, timestep)
-	}
-	sort.Ints(timesteps)
-
 	for R := R_start; R <= R_stop; R += d_R {
 		log.Println("table Ek - R:", R)
 		j := (R-R_start)/d_R + 1
 		table_str[0][j] = strconv.Itoa(R)
-		for i, timestep := range timesteps {
-			var ek float32 = 0
-			for _, atom := range dump.timesteps[timestep] {
-				dx := atom.x - y
-				dy := atom.y - x
-				dz := atom.z - zero_lvl
+		for i, timestep := range dump.timesteps {
+			var sum_ek float32
+			x := dump.extract("x", timestep)
+			y := dump.extract("y", timestep)
+			z := dump.extract("z", timestep)
+			ek := dump.extract("c_atom_ke", timestep)
+			for a_i := range x {
+				dx := x[a_i] - c_y
+				dy := y[a_i] - c_x
+				dz := z[a_i] - zero_lvl
 				m := dx*dx + dy*dy + dz*dz
-				if m <= float32(R*R) && atom.z <= zero_lvl+1 {
-					ek += atom.ek
+				if m <= float32(R*R) && z[a_i] <= zero_lvl+1 {
+					sum_ek += ek[a_i]
 				}
 			}
-			table_str[i+1][j] = strconv.FormatFloat(float64(ek), 'f', -1, 32)
+			table_str[i+1][j] = strconv.FormatFloat(float64(sum_ek), 'f', -1, 32)
 			table_str[i+1][0] = strconv.Itoa(timestep)
 		}
 	}
@@ -195,7 +209,7 @@ func main() {
 	dump_during := run_dir + "/dump.during"
 	log.Println("using run_dir:", run_dir)
 
-	dump := read_dump(dump_during)
+	dump := NewDump(dump_during)
 	zero_lvl := calc_zero_lvl(dump, 0)
 	center_x, center_y := calc_cluster_center(dump, 0)
 	log.Printf("zero_lvl: %f, center: (%f, %f)\n", zero_lvl, center_x, center_y)
